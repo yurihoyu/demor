da0b5905847923baee95c84ff710cf4019047143
YasukoDmm <112673301+yurihoyu@users.noreply.github.com>
2023-05-06 17:54:58 +0900

    質問内容

---
+++ src/main/java/com/example/demor/DemorApplication.java
@@ -0,0 +1,13 @@
+package com.example.demor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemorApplication.class, args);
+	}
+
+}
